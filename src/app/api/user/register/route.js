dc327daa677e9f246c281bb4b0cbacbcc3618e33
Renata Reinartz <112646473+renata1026@users.noreply.github.com>
2023-11-08 12:46:02 -0700

    create user routes

---
+++ src/app/api/user/register/route.js
@@ -0,0 +1,73 @@
+import prisma from 'src/lib/prisma';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export async function POST(req) {
+  try {
+    const { name, email, password, role } = req.body;
+
+    if (!name) {
+      return NextResponse.json({
+        success: false,
+        error: 'Please enter your name.',
+      });
+    }
+    if (!email) {
+      return NextResponse.json({
+        success: false,
+        error: 'Please enter your email.',
+      });
+    }
+    if (!password) {
+      return NextResponse.json({
+        success: false,
+        error: 'Please enter your password.',
+      });
+    }
+    if (!password) {
+      return NextResponse.json({
+        success: false,
+        error: 'Please enter your password.',
+      });
+    }
+
+    if (!role) {
+      return NextResponse.json({
+        success: false,
+        error: 'Please select a role.',
+      });
+    }
+
+    const checkUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (checkUser) {
+      return NextResponse.json({
+        success: false,
+        error: 'Email already exists, please login',
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+    return NextResponse.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: error.message,
+    });
+  }
+}
--- src/app/register/[id]/page.js
+++
@@ -1,7 +0,0 @@
-'use client';
-import { useParams } from 'next/navigation';
-export default function RegisterID() {
-  const params = useParams();
-  console.log(params.id);
-  return <div>My id register page</div>;
-}
