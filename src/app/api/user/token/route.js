0fd20760952894da14006d1ed019519520b510a5
Ryan Wilson <ryanjoelwilson@gmail.com>
2023-11-10 15:53:44 +0000

    build token route and improve middleware

--- src/app/api/user/token/route.js
+++ src/app/api/user/token/route.js
@@ -1,14 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "src/lib/prisma";
 
 export async function GET(req) {
   try {
-    const data = await req.json();
-    // console.log(data);
-    // if (!data.user) {
-    //   return NextResponse.json({ success: false, error: "please log in" });
-    // }
-    const user = data.user;
+    if (!req.user) {
+      return NextResponse.json({ success: false, error: "please log in" });
+    }
+    const user = req.user;
     return NextResponse.json({ success: true, user });
   } catch (error) {
     return NextResponse.json({ success: false, error: error.message });
--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,21 @@
 import prisma from "src/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 // import dotenv from "dotenv";
 // dotenv.config();
 
 export async function middleware(req, res) {
   try {
-    const data = req.json();
-    console.log(data.headers.authorization);
-    // console.log(req.headers);
     // Check if the request has an "Authorization" header
-    if (!req.headers.authorization) {
+    const header = req.headers.get("authorization");
+    if (!header) {
       console.log("doesn't exist?");
       return NextResponse.next();
     }
-    const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
+    const token = header.split(" ")[1];
     // Use the userId from the decoded token to find the corresponding user in the database
-    const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+    const testing = await jwtVerify(token, process.env.JWT_SECRET);
+    console.log(testing);
     const user = await prisma.user.findUnique({
       where: {
         id: userId,
@@ -30,6 +28,7 @@ export async function middleware(req, res) {
     // Remove the "password" property from the user object for security (assuming it's sensitive)
     delete user.password;
     // Attach the user object to the request object for later use in route handlers
+    console.log(user);
     const response = NextResponse.next();
     response.user = user;
     return response;
